6b0b4922cccdc4fd69efc4521dd5a680404c6810
funkeychang <funkeychang@gmail.com>
2017-05-31 15:33:33 +0800

    Add def is_jpg

--- ptt_beauty_crawler/ptt_beauty_crawler.py
+++ ptt_beauty_crawler/ptt_beauty_crawler.py
@@ -19,30 +19,38 @@ def check_dir(name):
         print("Creating folder %s ..." % name)
         os.mkdir(name)
         print("Done.")
+    else:
+        print("The folder %s exists! Start downloading ..." % name)
 
-def start_crawl(ptt_url):
-    res = requests.get(ptt_url)
+def is_jpg(file_name):
+    if file_name.split('.')[-1] == 'jpg':
+        return True
+
+def start_crawl():
+    beauty_url = get_url()
+    res = requests.get(beauty_url)
     soup = BeautifulSoup(res.text, "html.parser")
     pbar = ProgressBar()
     dir_name = soup.select('.article-meta-value')[2].text
     check_dir(dir_name)
-    #print("now in : " + os.getcwd())
     os.chdir(dir_name)
     #print("now in : " + os.getcwd())
 
     print("Now start to download pictures ...")
 
-    for link in pbar(soup.select('a')[5::2]):
-    	file_name = link['href'].split('/')[-1]
-    	res2 = requests.get(link['href'], stream=True)
-    	f = open(file_name, 'wb')
-    	shutil.copyfileobj(res2.raw, f)
-    	f.close()
-    	del res2
+    for a in pbar(soup.select('a')[5::]):
+        pic_link = a.text
+        file_name = pic_link.split('/')[-1]
+
+        if is_jpg(file_name):
+        	res2 = requests.get(pic_link, stream=True)
+        	f = open(file_name, 'wb')
+        	shutil.copyfileobj(res2.raw, f)
+        	f.close()
+        	del res2
 
     print("Pictures download finished!")
 
 if __name__ == '__main__':
     while True:
-        beauty_url = get_url()
-        start_crawl(beauty_url)
+        start_crawl()
