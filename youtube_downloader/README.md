d5ec4d78dc36d0a4c0795e40779c5682f6142c8f
funkeychang <funkeychang@gmail.com>
2017-06-07 10:41:02 +0800

    Update.

--- youtube_downloader/README.md
+++ youtube_downloader/README.md
@@ -2,4 +2,4 @@
 
 Download video from youtube.
 
-warning: It's still a prototype.
+2017.06.07: 有時候 video_source 會得到一個怪怪的 url 導致下載失敗，重新執行一次應該就正常了。
--- youtube_downloader/youtube_download_test.py
+++ youtube_downloader/youtube_download_test.py
@@ -7,27 +7,27 @@ import shutil
 from bs4 import BeautifulSoup
 
 # Get youtube video url
-url = input("Give me a youtube video url : ")
-
+youtube_url = input("Give me a youtube video url : ")
 # parse url to get video_info
-video_id = url.split('=')[-1]
+video_id = youtube_url.split('=')[-1]
 video_info = requests.get('https://www.youtube.com/get_video_info?video_id=' + video_id)
 
-# parse and decode "url_encoded_fmt_stream_map" information to get video soource url
-m = re.search('url_encoded_fmt_stream_map=(.*)', video_info.text)
-a = urllib.parse.parse_qs(m.group(0))
-time.sleep(2)
-b = urllib.parse.parse_qs(a['url_encoded_fmt_stream_map'][0])
+# first parse to get title and url_encoded_fmt_stream_map
+first_parse = urllib.parse.parse_qs(video_info.text)
+video_title = first_parse['title'][0]
+url_encoded_fmt_stream_map = first_parse['url_encoded_fmt_stream_map'][0]
 
-"""
-for q, u in zip(b['quality'], b['url']):
-    print(q, u)
-"""
+# parse again to get video_source like url and quality ... etc
+video_source = urllib.parse.parse_qs(url_encoded_fmt_stream_map)
+video_url = video_source['url']
+video_quality = video_source['quality']
 
 # start to download video
-print("Downloading ...")
-res = requests.get(b['url'][0], stream=True)
-f = open('test1.mp4', 'wb')
+print("Downloading video : " + video_title)
+
+# Always get video_url[0] cuz I am lazy :D
+res = requests.get(video_url[0], stream=True)
+f = open(video_title + '.mp4', 'wb')
 shutil.copyfileobj(res.raw, f)
 f.close()
 print("Finished.")
